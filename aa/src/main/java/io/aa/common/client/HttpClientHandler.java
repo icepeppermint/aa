01b6c533a0746b65b663222151f5ad22e1fe255a
icepeppermint <suyeong.hong@gmail.com>
2023-10-09 17:49:45 +0900

    Fix to support chunked response in HttpClientHandler

--- aa/src/main/java/io/aa/common/client/HttpClientHandler.java
+++ aa/src/main/java/io/aa/common/client/HttpClientHandler.java
@@ -2,11 +2,16 @@ package io.aa.common.client;
 
 import static java.util.Objects.requireNonNull;
 
+import javax.annotation.Nullable;
+
 import io.aa.common.HttpData;
 import io.aa.common.HttpRequest;
 import io.aa.common.HttpResponseWriter;
+import io.aa.common.ResponseHeaders;
 import io.aa.common.util.ChunkUtil;
+import io.aa.common.util.HttpUtil;
 import io.aa.common.util.NettyUtil;
+import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.handler.codec.http.HttpContent;
@@ -17,6 +22,8 @@ final class HttpClientHandler extends SimpleChannelInboundHandler<Object> {
 
     private final ClientRequestContext cctx;
     private final HttpResponseWriter res;
+    @Nullable
+    private ResponseHeaders headers;
 
     HttpClientHandler(ClientRequestContext cctx, HttpResponseWriter res) {
         this.cctx = requireNonNull(cctx, "cctx");
@@ -32,13 +39,18 @@ final class HttpClientHandler extends SimpleChannelInboundHandler<Object> {
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, Object msg) throws Exception {
         if (msg instanceof HttpResponse nettyRes) {
-            res.write(NettyUtil.toResponseHeaders(nettyRes));
+            res.write(headers = NettyUtil.toResponseHeaders(nettyRes));
         }
         if (msg instanceof HttpContent nettyBody) {
-            res.write(HttpData.of(ChunkUtil.fromChunk(nettyBody.content())));
+            ByteBuf content = nettyBody.content();
+            if (HttpUtil.isTransferEncodingChunked(headers)) {
+                content = ChunkUtil.fromChunk(content);
+            }
+            res.write(HttpData.of(content));
             if (msg instanceof LastHttpContent nettyTrailer) {
                 res.write(NettyUtil.toHttpHeaders(nettyTrailer.trailingHeaders()));
                 res.close();
+                headers = null;
             }
         }
     }
